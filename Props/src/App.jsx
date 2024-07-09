0bb9ccbd57e29f47549fa62e894e6bf2f835b9a8
nocooool <nakulshah3932@gmail.com>
2024-07-09 20:49:11 +0530

    hooks and tailwind

--- Props/src/App.jsx
+++ Props/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from 'src/assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import 'src/App.css'
 import Card from './components/card'
 
 function App() {
